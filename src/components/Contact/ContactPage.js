524453d3929a5ce4b11cf198e6e3b7b50b009ade
Vivek Mishra <vivekhere01@gmail.com>
2021-07-03 18:15:16 +0530

    css fixes

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -2,17 +2,37 @@ import React from 'react'
 import Fade from 'react-reveal/Fade'
 import './contact-page.css'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 function ContactPage() {
+
+    const showToast = () => {
+        return (
+            toast.dark('Message sent!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+    }
+
     return (
         <Fade>
-            <div className="contact-page my-5">
+            <div className="contact-page py-5 mb-5">
                 <div className="container">
-                    <h3>Contact / Feedback</h3>
+                    <ToastContainer />
+                    <h3 className="mb-5">Contact / Feedback</h3>
                     <form
                         name="contact"
                         method="POST"
                         data-netlify="true"
                         data-netlify-honeypot="bot-field"
+                        onSubmit={showToast}
                     >
                         <div className="row">
                             <div className="col-md-6 col-sm-12">
