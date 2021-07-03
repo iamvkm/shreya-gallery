4c27d3cbde957b9d636e9b17ca7e33f91dd60f7f
Vivek Mishra <vivekhere01@gmail.com>
2021-07-03 17:34:09 +0530

    css fixes

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -2,15 +2,57 @@ import React from 'react'
 import Fade from 'react-reveal/Fade'
 import './contact-page.css'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 function ContactPage() {
+
+    const showToast = () => {
+        return (
+            toast.dark('Message sent!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        )
+    }
+
     return (
         <Fade>
             <div className="contact-page">
                 <div className="container">
-                    <h1 className="mt-5">Contact</h1>
+                    <ToastContainer />
+                    <h3 className="mt-5">Contact / Feedback</h3>
+                    <form className="py-5" name="contact" method="POST" data-netlify="true" action={showToast}>
+                        <div className="row">
+                            <div className="col-md-6 col-sm-12">
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="name">Name</label>
+                                    <input type="name" id="name" class="form-control" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-sm-12">
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="email">Email</label>
+                                    <input type="email" id="email" class="form-control" />
+                                </div>
+                            </div>
+                            <div className="col-md-12 col-sm-12">
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="message">Message</label>
+                                    <textarea type="text" id="message" class="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
-        </Fade>
+        </Fade >
     )
 }
 
