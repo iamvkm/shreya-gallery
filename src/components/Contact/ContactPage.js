2dfa80a91383016a154a44124257881f879bf7db
Vivek Mishra <vivekhere01@gmail.com>
2021-07-03 17:54:27 +0530

    css fixes

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -2,36 +2,23 @@ import React from 'react'
 import Fade from 'react-reveal/Fade'
 import './contact-page.css'
 
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.min.css';
-
 function ContactPage() {
-
-    // const showToast = () => {
-    //     return (
-    //         toast.dark('Message sent!', {
-    //             position: "top-center",
-    //             autoClose: 5000,
-    //             hideProgressBar: false,
-    //             closeOnClick: true,
-    //             pauseOnHover: true,
-    //             draggable: true,
-    //             progress: undefined,
-    //         })
-    //     )
-    // }
-
     return (
         <Fade>
             <div className="contact-page my-5">
                 <div className="container">
                     <h3>Contact / Feedback</h3>
-                    <form className="py-4" name="contact" method="POST" data-netlify="true">
+                    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                        <input type="text" name="name" />
+                        <input type="email" name="email" />
+                        <textarea name="message"></textarea>
+                    </form>
+                    <form className="py-4" name="contact" method="POST">
                         <div className="row">
                             <div className="col-md-6 col-sm-12">
                                 <div class="form-outline mb-4">
                                     <label class="form-label" for="name">Name</label>
-                                    <input type="name" id="name" class="form-control" />
+                                    <input type="text" id="name" class="form-control" />
                                 </div>
                             </div>
                             <div className="col-md-6 col-sm-12">
@@ -43,7 +30,7 @@ function ContactPage() {
                             <div className="col-md-12 col-sm-12">
                                 <div class="form-outline mb-4">
                                     <label class="form-label" for="message">Message</label>
-                                    <textarea type="text" id="message" class="form-control" />
+                                    <textarea id="message" class="form-control" />
                                 </div>
                             </div>
                         </div>
