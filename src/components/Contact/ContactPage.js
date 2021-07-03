4b0809a7eb8bea3fd73226c68a062ca62d0fa09e
Vivek Mishra <vivekhere01@gmail.com>
2021-07-03 19:36:12 +0530

    css fixes

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -2,63 +2,73 @@ import React from 'react'
 import Fade from 'react-reveal/Fade'
 import './contact-page.css'
 
-import { useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-function ContactPage() {
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
 
-    useEffect(() => {
-        if (window.location.search.includes('success=true')) {
-            toast.dark('Message sent!', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
-        }
-    }, []);
+class ContactPage extends React.Component {
 
-    return (
-        <Fade>
-            <div className="contact-page py-5 mb-5">
-                <div className="container">
-                    <ToastContainer />
-                    <h3 className="mb-5">Contact / Feedback</h3>
-                    <form
-                        name="paintingContact"
-                        netlify
-                    >
-                        <input type="hidden" name="form-name" value="paintingContact" />
-                        <div className="row">
-                            <div className="col-md-6 col-sm-12">
-                                <div class="form-outline mb-4">
-                                    <label class="form-label" for="name">Name</label>
-                                    <input type="text" name="name" id="name" class="form-control" />
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+    }
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <Fade>
+                <div className="contact-page py-5 mb-5">
+                    <div className="container">
+                        <ToastContainer />
+                        <h3 className="mb-5">Contact / Feedback</h3>
+                        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
+                            <input type="hidden" name="form-name" value="contact" />
+                            <div className="row">
+                                <div className="col-md-6 col-sm-12">
+                                    <div class="form-outline mb-4">
+                                        <label class="form-label" for="name">Name</label>
+                                        <input type="text" name="name" id="name" class="form-control" value={name} onChange={this.handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <div class="form-outline mb-4">
+                                        <label class="form-label" for="email">Email</label>
+                                        <input type="email" name="email" id="email" class="form-control" value={email} onChange={this.handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-md-12 col-sm-12">
+                                    <div class="form-outline mb-4">
+                                        <label class="form-label" for="message">Message</label>
+                                        <textarea name="message" id="message" class="form-control" value={message} onChange={this.handleChange} />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-md-6 col-sm-12">
-                                <div class="form-outline mb-4">
-                                    <label class="form-label" for="email">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control" />
-                                </div>
-                            </div>
-                            <div className="col-md-12 col-sm-12">
-                                <div class="form-outline mb-4">
-                                    <label class="form-label" for="message">Message</label>
-                                    <textarea name="message" id="message" class="form-control" />
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </Fade >
-    )
+            </Fade >
+        )
+    }
 }
 
 export default ContactPage
