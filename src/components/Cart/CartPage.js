bf9e018f3422c687dd4fff45cf7edc254bf0b10c
Vivek Mishra <vivekhere01@gmail.com>
2021-07-04 12:45:27 +0530

    added form

--- src/components/Cart/CartPage.js
+++ src/components/Cart/CartPage.js
@@ -7,6 +7,7 @@ function CartPage({ cart, updateQuantity }) {
     var sumTotal = 0
 
     const initiatePayment = () => {
+
         var request = require('request');
 
         var headers = {
@@ -15,16 +16,24 @@ function CartPage({ cart, updateQuantity }) {
         }
 
         var payload = {
-            purpose: 'Paintings',
-            amount: sumTotal,
+            purpose: 'FIFA 16',
+            amount: '2500',
+            phone: '9999999999',
+            buyer_name: 'John Doe',
+            redirect_url: 'http://www.example.com/redirect/',
+            send_email: true,
+            webhook: 'http://www.example.com/webhook/',
+            send_sms: true,
+            email: 'foo@example.com',
+            allow_repeated_payments: false
         }
 
-        request.post('https://www.test.instamojo.com/api/1.1/payment-requests/',
-            { form: payload, headers: headers }, function (error, response, body) {
-                if (!error && response.statusCode === 201) {
-                    console.log(body);
-                } else alert(error)
-            })
+        request.post('https://www.instamojo.com/api/1.1/payment-requests/', { form: payload, headers: headers }, function (error, response, body) {
+            if (!error && response.statusCode == 201) {
+                console.log(body);
+            }
+        })
+
     }
 
     const renderItem = (itemInCart) => {
