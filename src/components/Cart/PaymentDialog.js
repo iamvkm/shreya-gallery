060dd633d9416176bc684315ed6875afefc90adc
Vivek Mishra <vivekhere01@gmail.com>
2021-07-05 21:00:18 +0530

    button and dialog fixes

--- src/components/Cart/PaymentDialog.js
+++ src/components/Cart/PaymentDialog.js
@@ -10,7 +10,7 @@ function PaymentDialog() {
                     lineHeight: '1.5'
                 }}
                     className="payment-dialog pt-5 my-5 mx-5">
-                    We are working on implementing payment gateways. For the time being, please reach out to me personally through the <a style={{textDecoration: 'none'}} href="/contact">contact form</a>.
+                    We are working on implementing payment gateways. For the time being, to place orders please reach out to me personally through the <a style={{ textDecoration: 'none' }} href="/contact">contact form</a>.
                 </h4>
                 <h5 style={{ color: '#303030', fontWeight: '700' }}>Apologies for the inconvenience 🙏</h5>
                 <a style={{ textDecoration: 'none' }} href="/">Go back to home</a>
